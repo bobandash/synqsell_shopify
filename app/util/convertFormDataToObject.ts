3e9f08c85603150a51bad2fc584ec555adfbb31c
bobandash <brucehsu1126@gmail.com>
2024-08-30 15:34:23 -0700

    Change: convert form data to automatically parse JSON string

--- app/util/convertFormDataToObject.ts
+++ app/util/convertFormDataToObject.ts
@@ -7,6 +7,12 @@ function convertFormDataToObject(formData: FormData): Record<string, any> {
       obj[key] = Number(value);
     } else if (Date.parse(value as string)) {
       obj[key] = new Date(value as string);
+    } else if (typeof value === 'string') {
+      try {
+        obj[key] = JSON.parse(value);
+      } catch {
+        obj[key] = value;
+      }
     } else {
       obj[key] = value;
     }
