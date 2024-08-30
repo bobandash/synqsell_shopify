84ab917c184a82f8682d8e36376d67eed53da9ba
bobandash <brucehsu1126@gmail.com>
2024-08-30 12:45:46 -0700

    Add: check list item navigation

--- app/routes/app._index/util/getChecklistBtnFunction.ts
+++ app/routes/app._index/util/getChecklistBtnFunction.ts
@@ -1,7 +1,7 @@
-import { type ShopifyGlobal } from "@shopify/app-bridge-react";
-import { MODALS } from "../constants";
-import { type NavigateFunction } from "@remix-run/react";
-import { CHECKLIST_ITEM_KEYS } from "~/constants";
+import { type ShopifyGlobal } from '@shopify/app-bridge-react';
+import { MODALS } from '../constants';
+import { type NavigateFunction } from '@remix-run/react';
+import { CHECKLIST_ITEM_KEYS } from '~/constants';
 
 type BtnAction = null | (() => void);
 
@@ -12,17 +12,30 @@ function getChecklistBtnFunction(
 ) {
   let btnFunction: BtnAction = null;
   switch (key) {
+    // retailer actions
     case CHECKLIST_ITEM_KEYS.RETAILER_GET_STARTED:
       btnFunction = () => openModal(shopify, MODALS.BECOME_RETAILER);
       break;
     case CHECKLIST_ITEM_KEYS.RETAILER_CUSTOMIZE_PROFILE:
-      btnFunction = () => navigate("/app/settings");
+      btnFunction = () => navigate('/app/settings');
+      break;
+    case CHECKLIST_ITEM_KEYS.RETAILER_REQUEST_PARTNERSHIP:
+      btnFunction = () => navigate('/app/supplier-network');
+      break;
+    case CHECKLIST_ITEM_KEYS.RETAILER_IMPORT_PRODUCT:
+      btnFunction = () => navigate('/app/products');
       break;
     case CHECKLIST_ITEM_KEYS.SUPPLIER_GET_STARTED:
       btnFunction = () => openModal(shopify, MODALS.BECOME_SUPPLIER);
       break;
     case CHECKLIST_ITEM_KEYS.SUPPLIER_CUSTOMIZE_PROFILE:
-      btnFunction = () => navigate("/app/settings");
+      btnFunction = () => navigate('/app/settings');
+      break;
+    case CHECKLIST_ITEM_KEYS.SUPPLIER_CREATE_PRICE_LIST:
+      btnFunction = () => navigate('/app/price-list');
+      break;
+    case CHECKLIST_ITEM_KEYS.SUPPLIER_EXPLORE_NETWORK:
+      btnFunction = () => navigate('/app/retailer-network');
       break;
   }
 
