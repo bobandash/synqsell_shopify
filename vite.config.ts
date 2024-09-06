bd01b36e50aa50815c2f9e54e868ae23c678c5c0
bobandash <brucehsu1126@gmail.com>
2024-09-06 11:55:09 -0700

    Add: remixDevTools for better logging during dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig, type UserConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { remixDevTools } from 'remix-development-tools';
 
 // Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
 // Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the remix server. The CLI will eventually
@@ -44,6 +45,7 @@ export default defineConfig({
     },
   },
   plugins: [
+    remixDevTools(),
     remix({
       ignoredRouteFiles: ['**/.*'],
     }),
@@ -51,5 +53,6 @@ export default defineConfig({
   ],
   build: {
     assetsInlineLimit: 0,
+    sourcemap: true,
   },
 }) satisfies UserConfig;
