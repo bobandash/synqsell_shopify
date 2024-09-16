27e0a28080eae2c766da7372d54f5fa668871bc7
bobandash <brucehsu1126@gmail.com>
2024-09-16 01:44:52 -0700

    Add: carrier service logic

---
+++ app/services/shopify/carrierService/graphql.ts
@@ -0,0 +1,48 @@
+export const CREATE_CARRIER_SERVICE = `#graphql 
+  mutation carrierServiceCreate($input: DeliveryCarrierServiceCreateInput!) {
+    carrierServiceCreate(input: $input) {
+      carrierService {
+        id
+        name
+      }
+      userErrors {
+        field
+        message
+      }
+    }
+  }
+`;
+
+export const GET_INITIAL_CARRIER_SERVICES = `#graphql
+  query initialCarrierServices {
+    carrierServices(first:5) {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`;
+
+export const GET_SUBSEQUENT_CARRIER_SERVICES = `#graphql
+  query subsequentCarrierServices($after: String!) {
+    carrierServices(after: $after, first:5) {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+`;
---
+++ app/services/shopify/carrierService/index.ts
@@ -0,0 +1,112 @@
+import { CARRIER_SERVICE_NAME } from '~/constants';
+import { errorHandler } from '~/services/util';
+import type { GraphQL } from '~/types';
+import {
+  CREATE_CARRIER_SERVICE,
+  GET_INITIAL_CARRIER_SERVICES,
+  GET_SUBSEQUENT_CARRIER_SERVICES,
+} from './graphql';
+import { getUserError } from '../util';
+
+type CarrierServiceDetail = {
+  id: string;
+  name: string;
+};
+
+// helper function for getCarrierService
+async function getAllCarrierServices(graphql: GraphQL) {
+  const carrierServices: CarrierServiceDetail[] = [];
+  let hasNextPage = true;
+  let isInitialFetch = true;
+  let endCursor = '';
+  do {
+    const query = isInitialFetch
+      ? GET_INITIAL_CARRIER_SERVICES
+      : GET_SUBSEQUENT_CARRIER_SERVICES;
+    const variables = isInitialFetch ? {} : { variables: { after: endCursor } };
+    const response = await graphql(query, variables);
+    const { data } = await response.json();
+    if (!data) {
+      throw new Error('Failed to get carrier service from shopify.');
+    }
+    const edgesData = Object.values(data)[0];
+    edgesData.edges.forEach(({ node }) => {
+      carrierServices.push({
+        id: node.id,
+        name: node.name ?? '',
+      });
+    });
+    endCursor = edgesData.pageInfo.endCursor ?? '';
+    hasNextPage = edgesData.pageInfo.hasNextPage;
+    isInitialFetch = false;
+  } while (hasNextPage);
+
+  return carrierServices;
+}
+
+export async function getCarrierService(sessionId: string, graphql: GraphQL) {
+  // retrieve all the carrier services
+  try {
+    const carrierServices = await getAllCarrierServices(graphql);
+    const filteredCarrierServices = carrierServices.filter(
+      ({ name }) => name === CARRIER_SERVICE_NAME,
+    );
+
+    if (filteredCarrierServices.length === 0) {
+      return null;
+    }
+    return filteredCarrierServices[0];
+  } catch (error) {
+    throw errorHandler(
+      error,
+      'Failed to get carrier service from shopify.',
+      getCarrierService,
+      { sessionId },
+    );
+  }
+}
+
+export async function createCarrierService(
+  sessionId: string,
+  graphql: GraphQL,
+) {
+  try {
+    const response = await graphql(CREATE_CARRIER_SERVICE, {
+      variables: {
+        input: {
+          active: true,
+          callbackUrl: process.env.CARRIER_SERVICE_CALLBACK_URL ?? '',
+          name: CARRIER_SERVICE_NAME,
+          supportsServiceDiscovery: false,
+        },
+      },
+    });
+
+    const { data } = await response.json();
+    const carrierServiceCreate = data?.carrierServiceCreate;
+    if (
+      !carrierServiceCreate ||
+      !carrierServiceCreate.carrierService ||
+      carrierServiceCreate.userErrors.length > 0
+    ) {
+      throw getUserError({
+        defaultMessage:
+          'Data is missing from creating the carrier service in Shpoify.',
+        userErrors: carrierServiceCreate?.userErrors,
+        parentFunc: createCarrierService,
+        data: { sessionId },
+      });
+    }
+    return {
+      id: carrierServiceCreate.carrierService.id,
+      name: carrierServiceCreate.carrierService.name,
+    };
+  } catch (error) {
+    throw errorHandler(
+      error,
+      'Failed to create carrier service in shopify.',
+      createCarrierService,
+      { sessionId },
+    );
+  }
+}
