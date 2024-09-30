b32cd40c119190e8a14f615ee39cd43332a01920
bobandash <brucehsu1126@gmail.com>
2024-09-29 22:56:30 -0700

    Add: payment method

--- app/routes/app.settings.payment/route.tsx
+++ app/routes/app.settings.payment/route.tsx
@@ -1,5 +1,15 @@
-import { Button, Card, Icon, InlineStack, Page, Text } from '@shopify/polaris';
-import { useCallback, useEffect, useState } from 'react';
+import {
+  BlockStack,
+  Button,
+  Card,
+  Form,
+  Icon,
+  InlineStack,
+  Layout,
+  Page,
+  Text,
+} from '@shopify/polaris';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { useFetcher, useLoaderData, useNavigate } from '@remix-run/react';
@@ -20,6 +30,9 @@ import {
 } from '~/services/models/stripeAccount';
 import { authenticate } from '~/shopify.server';
 import { CheckCircleIcon } from '@shopify/polaris-icons';
+import { useRoleContext } from '~/context/RoleProvider';
+import { Elements, PaymentElement } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 type LoaderData = {
   appBaseUrl: string;
@@ -31,6 +44,8 @@ type BeginStripeOnboardingFormData = {
   intent: string;
   appBaseUrl: string;
 };
+
+type StripeMode = 'payment' | 'setup' | 'subscription';
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   try {
@@ -74,17 +89,28 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 const PaymentSettings = () => {
   const navigate = useNavigate();
-  const { appBaseUrl, hasStripeAccountInDb } = useLoaderData<
-    typeof loader
-  >() as LoaderData;
+  const { isRetailer, isSupplier } = useRoleContext();
+  const { appBaseUrl, hasStripeAccountInDb, stripePublishableKey } =
+    useLoaderData<typeof loader>() as LoaderData;
   const [stripeOnboardingUrl, setStripeOnboardingUrl] = useState<string>('');
   const navigateUserSettings = useCallback(() => {
     navigate('/app/settings/user');
   }, [navigate]);
+  const stripePromise = useMemo(() => {
+    return loadStripe(stripePublishableKey);
+  }, [stripePublishableKey]);
+  const options = {
+    mode: 'payment' as StripeMode,
+    amount: 1099,
+    currency: 'usd',
+  };
 
   // form fetcher for handling stripe connect onboarding
   const beginStripeOnboardingFetcher = useFetcher({
     key: INTENTS.CREATE_ACCOUNT,
+  });
+  const createPaymentSourceStripeFetcher = useFetcher({
+    key: INTENTS.CREATE_PAYMENT_SOURCE,
   });
 
   const handleBeginOnboarding = useCallback(() => {
@@ -116,39 +142,68 @@ const PaymentSettings = () => {
 
   return (
     <Page
-      title={'Payment Integration'}
-      subtitle="Add stripe Integration to reliably disburse funds to suppliers if you're a retailer, or get paid as a retailer."
+      title={'Payment Integrations'}
+      subtitle="Add stripe integration to reliably disburse funds to suppliers if you're a retailer, or get paid as a retailer."
       primaryAction={{
         content: 'User Settings',
         helpText: 'Navigate to user profile settings.',
         onAction: navigateUserSettings,
       }}
     >
-      {!hasStripeAccountInDb ? (
-        <div>
-          <Button
-            variant={'primary'}
-            onClick={handleBeginOnboarding}
-            disabled={beginStripeOnboardingFetcher.state === 'submitting'}
+      <Layout>
+        {isRetailer && (
+          <Layout.AnnotatedSection
+            id="retailerPaymentMethod"
+            title="Payment Method (Retailer)"
+            description="Add a payment method to securely pay suppliers with Stripe."
           >
-            {beginStripeOnboardingFetcher.state === 'submitting'
-              ? 'Creating a connected account'
-              : 'Start Onboarding Process'}
-          </Button>
-        </div>
-      ) : (
-        // TODO: Figure out what to display for this
-        <Card>
-          <InlineStack gap="100">
-            <div>
-              <Icon source={CheckCircleIcon} tone="base" />
-            </div>
-            <Text variant="bodyLg" as="h2">
-              Stripe Connect has been successfully integrated.
-            </Text>
-          </InlineStack>
-        </Card>
-      )}
+            <Card>
+              <Form onSubmit={() => {}}>
+                <BlockStack gap="200">
+                  <Elements stripe={stripePromise} options={options}>
+                    <PaymentElement />
+                  </Elements>
+                  <InlineStack align="end">
+                    <Button variant="primary">Add Payment Method</Button>
+                  </InlineStack>
+                </BlockStack>
+              </Form>
+            </Card>
+          </Layout.AnnotatedSection>
+        )}
+        {isSupplier && (
+          <Layout.AnnotatedSection
+            id="supplierPaymentMethod"
+            title="Stripe Connect (Supplier)"
+            description="Onboard with stripe to receive payments."
+          >
+            {!hasStripeAccountInDb ? (
+              <div>
+                <Button
+                  variant={'primary'}
+                  onClick={handleBeginOnboarding}
+                  disabled={beginStripeOnboardingFetcher.state === 'submitting'}
+                >
+                  {beginStripeOnboardingFetcher.state === 'submitting'
+                    ? 'Creating a connected account'
+                    : 'Start Onboarding Process'}
+                </Button>
+              </div>
+            ) : (
+              <Card>
+                <InlineStack gap="100">
+                  <div>
+                    <Icon source={CheckCircleIcon} tone="base" />
+                  </div>
+                  <Text variant="bodyLg" as="h2">
+                    Stripe Connect has been successfully integrated.
+                  </Text>
+                </InlineStack>
+              </Card>
+            )}
+          </Layout.AnnotatedSection>
+        )}
+      </Layout>
     </Page>
   );
 };
