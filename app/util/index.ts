e06aa0bb3184051007d72897e601b05ce8349242
bobandash <brucehsu1126@gmail.com>
2024-10-01 11:11:47 -0700

    Remove: unused util functions

--- app/util/index.ts
+++ app/util/index.ts
@@ -1,5 +1,4 @@
 export { default as convertObjectValuesToArr } from './convertObjectValuesToArr';
 export { default as convertFormDataToObject } from './convertFormDataToObject';
 export { default as getJSONError } from './getJSONError';
-export { default as convertIsoStringToDateFmt } from './convertIsoStringToDateFmt';
 export { default as createJSONMessage } from './createJSONMessage';
