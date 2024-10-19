b054cb1106a26d5cf7556b0cc7f7d140e7e84bb3
Bruce Hsu <74850332+bobandash@users.noreply.github.com>
2024-10-19 09:48:33 -0700

    Add: handle impl for account.application.deauthorization

--- aws/stripe/webhooks/accountApplicationDeauthorized/app.ts
+++ aws/stripe/webhooks/accountApplicationDeauthorized/app.ts
@@ -1,31 +1,47 @@
 import { APIGatewayProxyResult } from 'aws-lambda';
 import { PoolClient } from 'pg';
 import { initializePool } from './db';
+import { Event } from './types';
 
-export const lambdaHandler = async (event: any): Promise<APIGatewayProxyResult> => {
-    // let client: null | PoolClient = null;
+async function removeStripeAccountFromDatabase(accountId: string, client: PoolClient) {
     try {
-        // const pool = initializePool();
-        // client = await pool.connect();
-        console.log(event);
+        const query = `
+            DELETE FROM "StripeConnectAccount"
+            WHERE "stripeAccountId" = $1
+        `;
+        await client.query(query, [accountId]);
+    } catch (error) {
+        console.error(error);
+        throw new Error(`Failed to remove stripe account ${accountId} from database.`);
+    }
+}
 
+export const lambdaHandler = async (event: Event): Promise<APIGatewayProxyResult> => {
+    let client: null | PoolClient = null;
+    const accountId = event.account;
+
+    try {
+        const pool = initializePool();
+        client = await pool.connect();
+        await removeStripeAccountFromDatabase(accountId, client);
         return {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'Successfully did procedure.',
+                message: `Successfully removed account ${accountId}.`,
             }),
         };
     } catch (error) {
+        console.error(error);
         return {
             statusCode: 500,
             body: JSON.stringify({
-                message: 'Could not delete products.',
+                message: `Failed to remove account ${accountId} from database.`,
                 error: (error as Error).message,
             }),
         };
     } finally {
-        // if (client) {
-        //     client.release();
-        // }
+        if (client) {
+            client.release();
+        }
     }
 };
