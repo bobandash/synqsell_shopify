17ade57c46c62d4904990d7116e21dcfcf6ac0f6
Bruce Hsu <74850332+bobandash@users.noreply.github.com>
2024-10-19 21:02:07 -0700

    Add: todo item

--- application/app/constants.ts
+++ application/app/constants.ts
@@ -4,16 +4,11 @@ export const ROLES = {
   ADMIN: 'admin',
 };
 
-export type RolesOptionsProps = (typeof ROLES)[keyof typeof ROLES];
-
 export const ACCESS_REQUEST_STATUS = {
   REJECTED: 'rejected',
   PENDING: 'pending',
   APPROVED: 'approved',
 } as const;
-
-export type AccessRequestStatusOptionsProps =
-  (typeof ACCESS_REQUEST_STATUS)[keyof typeof ACCESS_REQUEST_STATUS];
 
 export const CHECKLIST_ITEM_KEYS = {
   RETAILER_GET_STARTED: 'retailer_get_started',
@@ -29,33 +24,21 @@ export const CHECKLIST_ITEM_KEYS = {
   SUPPLIER_EXPLORE_NETWORK: 'supplier_explore_network',
 } as const;
 
-export type ChecklistItemKeysOptionsProps =
-  (typeof CHECKLIST_ITEM_KEYS)[keyof typeof CHECKLIST_ITEM_KEYS];
-
 // constants that have to do with price list
 export const PRICE_LIST_CATEGORY = {
   GENERAL: 'GENERAL',
   PRIVATE: 'PRIVATE',
 } as const;
 
-export type PriceListCategoryOptionsProps =
-  (typeof PRICE_LIST_CATEGORY)[keyof typeof PRICE_LIST_CATEGORY];
-
 export const PRICE_LIST_IMPORT_SETTINGS = {
   NO_APPROVAL: 'NO_APPROVAL',
   APPROVAL: 'APPROVAL',
 } as const;
 
-export type PriceListImportSettingsOptionsProps =
-  (typeof PRICE_LIST_IMPORT_SETTINGS)[keyof typeof PRICE_LIST_IMPORT_SETTINGS];
-
 export const PRICE_LIST_PRICING_STRATEGY = {
   WHOLESALE: 'WHOLESALE',
   MARGIN: 'MARGIN',
 } as const;
-
-export type PriceListPricingStrategyOptionProps =
-  (typeof PRICE_LIST_PRICING_STRATEGY)[keyof typeof PRICE_LIST_PRICING_STRATEGY];
 
 export const FULFILLMENT_SERVICE = {
   name: 'SynqSell',
@@ -81,6 +64,24 @@ export const ORDER_PAYMENT_STATUS = {
   PARTIALLY_PAID: 'PARTIALLY_PAID',
   PAID: 'PAID',
 } as const;
+
+// TODO: put this in types.ts
+export type RolesOptionsProps = (typeof ROLES)[keyof typeof ROLES];
+
+export type AccessRequestStatusOptionsProps =
+  (typeof ACCESS_REQUEST_STATUS)[keyof typeof ACCESS_REQUEST_STATUS];
+
+export type ChecklistItemKeysOptionsProps =
+  (typeof CHECKLIST_ITEM_KEYS)[keyof typeof CHECKLIST_ITEM_KEYS];
+
+export type PriceListCategoryOptionsProps =
+  (typeof PRICE_LIST_CATEGORY)[keyof typeof PRICE_LIST_CATEGORY];
+
+export type PriceListImportSettingsOptionsProps =
+  (typeof PRICE_LIST_IMPORT_SETTINGS)[keyof typeof PRICE_LIST_IMPORT_SETTINGS];
+
+export type PriceListPricingStrategyOptionProps =
+  (typeof PRICE_LIST_PRICING_STRATEGY)[keyof typeof PRICE_LIST_PRICING_STRATEGY];
 
 export type PartnershipRequestTypeProps =
   (typeof PARTNERSHIP_REQUEST_TYPE)[keyof typeof PARTNERSHIP_REQUEST_TYPE];
