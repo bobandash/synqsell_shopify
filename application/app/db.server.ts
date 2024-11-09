4e543839e4adcc22b141c9298877e7d958db93ca
Bruce Hsu <74850332+bobandash@users.noreply.github.com>
2024-11-08 16:27:54 -0800

    Add: env to task definition for ecs to run properly

--- application/app/db.server.ts
+++ application/app/db.server.ts
@@ -1,72 +1,32 @@
 import { PrismaClient } from '@prisma/client';
-import { errorHandler, getSecrets } from './lib/utils/server';
-
-type DatabaseUserCredentials = {
-  password: string;
-  username: string;
-};
-type DatabaseConnection = {
-  database: string;
-  host: string;
-  port: string;
-};
-
 declare global {
   var prisma: PrismaClient;
 }
 
 const prisma: PrismaClient = global.prisma || new PrismaClient();
 
-async function getProductionDatabaseUrl() {
-  try {
-    const dbUserCredentialsArn =
-      process.env.DATABASE_USER_CREDENTIALS_ARN ?? '';
-    const dbConnectionArn = process.env.DATABASE_CONNECTION_ARN ?? '';
-    const [userCredentialSecrets, dbConnectionSecrets] = await Promise.all([
-      getSecrets<DatabaseUserCredentials>(dbUserCredentialsArn),
-      getSecrets<DatabaseConnection>(dbConnectionArn),
-    ]);
-    const { username, password } = userCredentialSecrets;
-    const { host, port, database } = dbConnectionSecrets;
-    const databaseUrl = `postgresql://${username}:${password}@${host}:${port}/${database}`;
-    return databaseUrl;
-  } catch (error) {
-    throw errorHandler(
-      error,
-      'Failed to get production database url.',
-      getProductionDatabaseUrl,
-    );
+function getDatabaseUrl() {
+  if (
+    !process.env.USERNAME ||
+    !process.env.PASSWORD ||
+    !process.env.HOST ||
+    !process.env.PORT ||
+    !process.env.DATABASE
+  ) {
+    throw new Error('Missing certain database connection parameters.');
   }
-}
-
-function getDevelopmentDatabaseUrl() {
-  const databaseUrl = process.env.DATABASE_URL;
-  if (!databaseUrl) {
-    throw new Error('Failed to get development database url.');
-  }
+  const databaseUrl = `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
   return databaseUrl;
 }
 
-async function getDatabaseUrl() {
-  if (process.env.NODE_ENV === 'development') {
-    return getDevelopmentDatabaseUrl();
-  } else if (process.env.NODE_ENV === 'production') {
-    return await getProductionDatabaseUrl();
-  }
-  throw new Error(
-    'Unhandled. Environment is not valid for retrieving database url.',
-  );
-}
-
 if (!global.prisma) {
-  getDatabaseUrl().then((databaseUrl) => {
-    global.prisma = new PrismaClient({
-      datasources: {
-        db: {
-          url: databaseUrl,
-        },
+  const databaseUrl = getDatabaseUrl();
+  global.prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl,
       },
-    });
+    },
   });
 }
 
