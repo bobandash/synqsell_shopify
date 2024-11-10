a14158df83f19d4f5ef1788a33b4865f77494f44
bobandash <brucehsu1126@gmail.com>
2024-11-10 01:09:19 -0800

    Refactor: use database url process env

--- application/app/db.server.ts
+++ application/app/db.server.ts
@@ -5,26 +5,11 @@ declare global {
 
 const prisma: PrismaClient = global.prisma || new PrismaClient();
 
-function getDatabaseUrl() {
-  if (
-    !process.env.USERNAME ||
-    !process.env.PASSWORD ||
-    !process.env.HOST ||
-    !process.env.PORT ||
-    !process.env.DATABASE
-  ) {
-    throw new Error('Missing certain database connection parameters.');
-  }
-  const databaseUrl = `postgresql://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
-  return databaseUrl;
-}
-
 if (!global.prisma) {
-  const databaseUrl = getDatabaseUrl();
   global.prisma = new PrismaClient({
     datasources: {
       db: {
-        url: databaseUrl,
+        url: process.env.DATABASE_URL,
       },
     },
   });
