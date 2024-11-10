e997008bd0fe029f37214e4015d6f9ca437add07
bobandash <brucehsu1126@gmail.com>
2024-11-10 01:16:00 -0800

    Fix: specify username, password, host env to have db prefix

--- application/entrypoint.sh
+++ application/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export DATABASE_URL="postgresql://${USERNAME}:${PASSWORD}@${HOST}:${PORT}/${DATABASE}"
+export DATABASE_URL="postgresql://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DATABASE}"
 exec "$@"
