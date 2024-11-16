54542ad04051b684ac999a61dfb1adb4d2ed4e9a
bobandash <brucehsu1126@gmail.com>
2024-11-16 12:40:32 -0800

    Remove: logger from domain errors (client generated)

--- application/app/lib/utils/server/createJSONError.server.ts
+++ application/app/lib/utils/server/createJSONError.server.ts
@@ -1,9 +1,7 @@
 import { json } from '@remix-run/node';
-import logger from '~/logger';
 
 // this is for explicit error messages inside the loader or action
 function createJSONError(message: string, statusCode: number) {
-  logger.error(message);
   return json(
     {
       error: {
