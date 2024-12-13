5479f9d501ffc10f540a05df1a8c1b93fdbb57b0
bobandash <brucehsu1126@gmail.com>
2024-12-13 00:24:08 -0800

    Add: just warn the entire event

--- aws/shopify/webhooks/customersDataRequest/app.ts
+++ aws/shopify/webhooks/customersDataRequest/app.ts
@@ -1,22 +1,8 @@
-import { APIGatewayProxyResult } from "aws-lambda";
 import { ShopifyEvent } from "./types";
 
 export const lambdaHandler = async (
   event: ShopifyEvent
-): Promise<APIGatewayProxyResult> => {
-  console.warn("Compliance event", {
-    type: "customers/data_request",
-    shopId: event.shop_id,
-    requestId: event.data_request,
-    shop_domain: event.shop_domain,
-    customerId: event.customer.id,
-    orders: event.orders_requested,
-  });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message:
-        "SynqSell currently does not store any customer data in its database.",
-    }),
-  };
+) => {
+  console.warn("Compliance event",  event);
+  return;
 };
