ec338d865372d39c34ef29a03e5d500c27477117
bobandash <brucehsu1126@gmail.com>
2024-12-30 11:46:58 -0800

    Fix: map function

--- aws/util-layer/utils/createMapToRestObj.ts
+++ aws/util-layer/utils/createMapToRestObj.ts
@@ -1,7 +1,7 @@
-function createMapIdToRestObj<
-  T extends Record<string, string>,
-  K extends keyof T
->(data: T[], idKey: K) {
+function createMapIdToRestObj<T extends Record<string, any>, K extends keyof T>(
+  data: T[],
+  idKey: K
+) {
   const map = new Map<string, Omit<T, K>>();
   data.forEach((entry) => {
     const id = entry[idKey];
