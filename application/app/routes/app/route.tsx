19485398a0b4ddbb755e3a9245c7b01b2627e8ab
bobandash <brucehsu1126@gmail.com>
2025-01-23 16:05:17 -0800

    Add: temporarily make all users who install app into an admin

--- application/app/routes/app/route.tsx
+++ application/app/routes/app/route.tsx
@@ -11,7 +11,7 @@ import { AppProvider } from '@shopify/shopify-app-remix/react';
 import { NavMenu } from '@shopify/app-bridge-react';
 import polarisStyles from '@shopify/polaris/build/esm/styles.css?url';
 import { authenticate } from '../../shopify.server';
-import { getRoles } from '~/services/models/roles.server';
+import { addRole, getRoles } from '~/services/models/roles.server';
 import { ROLES } from '~/constants';
 import { useEffect, useState } from 'react';
 import { RoleProvider } from '~/context/RoleProvider';
@@ -42,6 +42,8 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   // this has to be outside cause the onFailure will be wrapped by the try catch
   await requireBilling(shop, isTest, billing);
   await addBillingToDatabase(sessionId, isTest, billing);
+  // TODO: DELETE AFTER PROD IS APPROVED, SHOPIFY WANTS TO TEST APPLICATION IN PROD NOT STAGING
+  await addRole(sessionId, ROLES.ADMIN);
   const [roles, hasStripeConnectAccount, hasStripePaymentMethod] =
     await Promise.all([
       getRoles(sessionId),
